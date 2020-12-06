81cfff8b4b8facbfc1b2fc582f54be32448b4f63
pomkos <albeithome@gmail.com>
2020-12-06 11:40:58 -0500

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -5,8 +5,5 @@ db_ip="CHANGE ME"   # Database local ip such as 192.168.1.11, without http. Quot
 db_port="CHANGE ME" # Database port such as 5432. Quotes not included
 
 # start script |   folder    |  conda_env  | port |          database info         |     log file location
-./st_starter.sh -f payme      -e tip_env   -p 8501 -u $db_user -i $db_ip -r $db_port >> ~/scripts/log_pay.txt
-./st_starter.sh -f brotein    -e bro_env   -p 8502 -u $db_user -i $db_ip -r $db_port >> ~/scripts/log_bro.txt
-./st_starter.sh -f covid_dash -e covid_env -p 8503 -u $db_user -i $db_ip -r $db_port >> ~/scripts/log_covid.txt
-./st_starter.sh -f portfolio  -e port_env  -p 8504 -u $db_user -i $db_ip -r $db_port >> ~/scripts/log_port.txt
-
+./st_starter.sh -f my_dash    -e covid_env -p 8501 -u $db_user -i $db_ip -r $db_port >> ~/streamlit_starter/log_covid.txt
+./st_starter.sh -f portfolio  -e port_env  -p 8502 -u $db_user -i $db_ip -r $db_port >> ~/streamlit_starter/log_port.txt
