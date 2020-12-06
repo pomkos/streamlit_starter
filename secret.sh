820b408f9a53f3916d83f939ad29f3655c522752
pomkos <albeithome@gmail.com>
2020-12-06 11:27:30 -0500

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-db_user="CHANGE ME" # Database  login in the format of "user:pw", quotes included
-db_ip="CHANGE ME"   # Database local ip such as 192.168.1.11, without http
-db_port="CHANGE ME" # Database port such as 5432
+db_user="CHANGE ME" # Database  login in the format of "user:pw". Quotes included
+db_ip="CHANGE ME"   # Database local ip such as 192.168.1.11, without http. Quotes not included
+db_port="CHANGE ME" # Database port such as 5432. Quotes not included
 
 # start script |   folder    |  conda_env  | port |          database info         |     log file location
 ./st_starter.sh -f payme      -e tip_env   -p 8501 -u $db_user -i $db_ip -r $db_port >> ~/scripts/log_pay.txt
