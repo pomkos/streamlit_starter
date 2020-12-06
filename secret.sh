f3ff4caa86c941bd73b9d5896648a9b237282ecb
pomkos <albeithome@gmail.com>
2020-12-06 11:16:50 -0500

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -4,7 +4,7 @@ db_user="CHANGE ME" # Database  login in the format of "user:pw", quotes include
 db_ip="CHANGE ME"   # Database local ip such as 192.168.1.11, without http
 db_port="CHANGE ME" # Database port such as 5432
 
-# start script     folder       conda_env     port     [       database info           ]  log file location
+# start script |   folder    |  conda_env  | port |          database info         |     log file location
 ./st_starter.sh -f payme      -e tip_env   -p 8501 -u $db_user -i $db_ip -r $db_port >> ~/scripts/log_pay.txt
 ./st_starter.sh -f brotein    -e bro_env   -p 8502 -u $db_user -i $db_ip -r $db_port >> ~/scripts/log_bro.txt
 ./st_starter.sh -f covid_dash -e covid_env -p 8503 -u $db_user -i $db_ip -r $db_port >> ~/scripts/log_covid.txt
