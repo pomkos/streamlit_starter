c2159a43afce687d3f26e1c5769621d3d0166706
pomkos <albeithome@gmail.com>
2020-12-06 00:14:22 -0500

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-db_user="CHANGE ME"
-db_ip="CHANGE ME"
-db_port="CHANGE ME"
+db_user="CHANGE ME" # in the format of user:pw
+db_ip="CHANGE ME" # such as 192.168.1.11
+db_port="CHANGE ME" # such as 5432
 
 ./st_starter.sh -f payme -e tip_env -p 8501 -u $db_user -i $db_ip -r $db_port >> ~/scripts/log_pay.txt
 ./st_starter.sh -f brotein -e bro_env -p 8502 -u $db_user -i $db_ip -r $db_port >> ~/scripts/log_bro.txt
